4948d028aaee1eadddd10d8d7493011beca9dc21
itamarh-gd <itamar.h@glidingdeer.com>
2026-06-17 15:55:51 +0300

    cleaner distribution of values in results as object and not array

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// take the input
-// calculate all the values
-// render the results to the elements
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
 let unitValue = 0
 
 const convertBtn = document.getElementById("convert-btn")
@@ -27,12 +19,12 @@ function calculate(unitValue) {
         const gallon2Liter = (unitValue / 0.264).toFixed(3)
         const kg2Pound = (unitValue * 2.204).toFixed(3)
         const pound2Kg = (unitValue / 2.204).toFixed(3)
-        const results = [meter2Feet, feet2Meter, liter2Gallon, gallon2Liter, kg2Pound, pound2Kg]
+        const results = {meter2Feet, feet2Meter, liter2Gallon, gallon2Liter, kg2Pound, pound2Kg}
         return results
 }
 
 function renderResults(results) {
-        lengthEl.innerText = `${unitValue} meters = ${results[0]} feet | ${unitValue} feet = ${results[1]} meters`
-        volumeEl.innerText = `${unitValue} liters = ${results[2]} gallons | ${unitValue} gallons = ${results[3]} liters`
-        massEl.innerText = `${unitValue} kilograms = ${results[4]} pounds | ${unitValue} pounds = ${results[5]} kilograms`
+        lengthEl.innerText = `${unitValue} meters = ${results.meter2Feet} feet | ${unitValue} feet = ${results.feet2Meter} meters`
+        volumeEl.innerText = `${unitValue} liters = ${results.liter2Gallon} gallons | ${unitValue} gallons = ${results.gallon2Liter} liters`
+        massEl.innerText = `${unitValue} kilograms = ${results.kg2Pound} pounds | ${unitValue} pounds = ${results.pound2Kg} kilograms`
 }
